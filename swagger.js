1bface2faf88e47d4ac45119a61c73344b694869
calvinvanoss <cvanoss@calpoly.edu>
2024-05-22 10:57:32 -0700

    autogen swagger

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "Recipe API",
+  },
+};
+
+const outputFile = "./swagger.json";
+const routes = ["./src/app.ts"];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
