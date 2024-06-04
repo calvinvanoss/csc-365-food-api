21dea271f6ddf4812221d419ca757f081c41ad09
calvinvanoss <cvanoss@calpoly.edu>
2024-06-04 11:26:16 -0700

    million row script

--- scripts/autopopulate.ts
+++ scripts/autopopulate.ts
@@ -3,129 +3,79 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const exampleIngredients = [
-  "flour",
-  "sugar",
-  "butter",
-  "eggs",
-  "milk",
-  "chocolate",
-  "vanilla",
-  "salt",
-  "baking soda",
-  "baking powder",
-  "cinnamon",
-  "nutmeg",
-  "ginger",
-  "cocoa powder",
-  "honey",
-  "maple syrup",
-  "olive oil",
-  "vegetable oil",
-  "coconut oil",
-  "peanut butter",
-];
-
-const exampleAttributes = [
-  "vegan",
-  "vegetarian",
-  "gluten-free",
-  "dairy-free",
-  "nut-free",
-  "egg-free",
-  "soy-free",
-  "kosher",
-  "halal",
-];
+const randomNumSet = (length: number, range: number) => {
+  const set = new Set<number>();
+  while (set.size < length) {
+    const randomNum = faker.number.int({ min: 1, max: range });
+    set.add(randomNum);
+  }
+  return Array.from(set);
+};
 
 const autopopulate = async () => {
-  // add example ingredients
-  for (const name of exampleIngredients) {
-    await prisma.ingredient.upsert({
-      where: { name },
-      update: {},
-      create: { name },
-    });
-  }
-  // add example attributes
-  for (const name of exampleAttributes) {
-    await prisma.attribute.upsert({
-      where: { name },
-      update: {},
-      create: { name },
-    });
-  }
+  await prisma.ingredient.createMany({
+    data: Array.from({ length: 100 }).map((_, idx) => ({
+      name: `ingredient ${idx + 1}`,
+    })),
+  });
 
-  // add 10 random users
-  const users = await Promise.all(
-    Array.from({ length: 10 }).map(
-      async () =>
-        await prisma.user.create({
-          data: {
-            name: faker.internet.userName(),
-            password: faker.internet.password(),
-            token: "",
-          },
-        }),
-    ),
-  );
+  await prisma.attribute.createMany({
+    data: Array.from({ length: 40 }).map((_, idx) => ({
+      name: `attribute ${idx + 1}`,
+    })),
+  });
 
-  // each user creates 2 recipes with up to 5 random ingredients and 3 random attributes
-  for (const user of users) {
-    for (let i = 0; i < 2; i++) {
-      const ingredientIds = new Set<number>(
-        Array.from({ length: 5 }).map(() =>
-          faker.number.int({ min: 1, max: 20 }),
-        ),
-      );
-      const attributeIds = new Set<number>(
-        Array.from({ length: 3 }).map(() =>
-          faker.number.int({ min: 1, max: 9 }),
-        ),
-      );
+  await prisma.user.createMany({
+    data: Array.from({ length: 50000 }).map((_, idx) => ({
+      name: `user ${idx + 1}`,
+      password: "password",
+      token: `token ${idx + 1}`,
+    })),
+  });
 
-      await prisma.recipe.create({
-        data: {
-          name: faker.lorem.words(3),
+  await prisma.recipe.createMany({
+    data: Array.from({ length: 50000 })
+      .map((_, idx) => ({
+        name: `recipe1 ${idx + 1}`,
+        instructions: faker.lorem.sentence(),
+        userId: idx + 1,
+      }))
+      .concat(
+        Array.from({ length: 50000 }).map((_, idx) => ({
+          name: `recipe2 ${idx + 1}`,
           instructions: faker.lorem.sentence(),
-          userId: user.id,
-          recipeIngredients: {
-            createMany: {
-              data: [...ingredientIds].map((id) => ({
-                ingredientId: id,
-              })),
-            },
-          },
-          recipeAttributes: {
-            createMany: {
-              data: [...attributeIds].map((id) => ({
-                attributeId: id,
-              })),
-            },
-          },
-        },
-      });
-    }
-  }
-
-  // each user rates up to 5 random recipes
-  for (const user of users) {
-    [
-      ...new Set<number>(
-        Array.from({ length: 5 }).map(() =>
-          faker.number.int({ min: 1, max: 20 }),
-        ),
+          userId: idx + 1,
+        })),
       ),
-    ].forEach(async (id) => {
-      await prisma.rating.create({
-        data: {
-          rating: faker.number.int({ min: 1, max: 5 }),
-          userId: user.id,
-          recipeId: id,
-        },
-      });
-    });
-  }
+  });
+
+  await prisma.recipeIngredient.createMany({
+    data: Array.from({ length: 100000 }).flatMap((_, idx) =>
+      randomNumSet(5, 100).map((ingredientId) => ({
+        recipeId: idx + 1,
+        ingredientId,
+      })),
+    ),
+  });
+
+  await prisma.recipeAttribute.createMany({
+    data: Array.from({ length: 100000 }).flatMap((_, idx) =>
+      randomNumSet(2, 40).map((attributeId) => ({
+        recipeId: idx + 1,
+        attributeId,
+      })),
+    ),
+  });
+
+  await prisma.rating.createMany({
+    data: Array.from({ length: 50000 }).flatMap((_, idx) =>
+      randomNumSet(10, 100000).map((recipeId) => ({
+        recipeId,
+        userId: idx + 1,
+        rating: faker.number.int({ min: 1, max: 5 }),
+      })),
+    ),
+  });
 };
 
 autopopulate();
